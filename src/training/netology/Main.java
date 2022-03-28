7fa23c8737f7b4b0cf9da15aa1cb9765471222ce
Raconocar <Stranger_forever_rus112@rambler.ru>
2022-03-28 21:48:59 +0300

    Update Main.java
    Clean code

--- src/training/netology/Main.java
+++ src/training/netology/Main.java
@@ -10,7 +10,6 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
         Collections.sort(intList, Integer::compareTo);
-//        System.out.println(intList);
         for (int x : intList) {
             if (x > 0 && x % 2 == 0)
                 System.out.println(x);
