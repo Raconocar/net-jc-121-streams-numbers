116def648dfebc37ad85c641bb8a374e8277a73d
Raconocarn <Stranger_forever_rus112@rambler.ru>
2022-03-28 21:47:55 +0300

    Initial commit

---
+++ src/training/netology/Main.java
@@ -0,0 +1,21 @@
+package training.netology;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        Collections.sort(intList, Integer::compareTo);
+//        System.out.println(intList);
+        for (int x : intList) {
+            if (x > 0 && x % 2 == 0)
+                System.out.println(x);
+        }
+
+
+    }
+}
